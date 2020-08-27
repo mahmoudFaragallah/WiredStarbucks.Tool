189916b8201bb94e665cbbdd68a362c6b6d16ff7
Mahmoud Faragallah <mahmoudahmedfarajallah@outlook.com>
2020-08-27 20:47:14 +0200

    Add project

---
+++ WiredStarbucks.Tool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WiredStarbucks.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Alexandria");
+        }
+    }
+}
